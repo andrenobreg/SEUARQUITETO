e001a49ad77708a023a5e61f4630cae3a3d1e4a4
André Nóbrega <andrenobreg@gmail.com>
2021-05-09 14:13:12 -0300

    Mecanismo da barra busca

--- src/barracidades.js
+++ src/barracidades.js
@@ -1,21 +1,18 @@
 import React, { Component } from 'react';
 import './css/barracidade.css';
 import setabaixo from './img/setabaixo.png';
-
-/*
-class Barracidades extends Component {
-    constructor (props){
-        super(props);
-        this.myFunction = this.myFunction.bind(this);
-
-    }
-    
-    let cidade = ["BRASIL","ARACAJU","BELÉM","BELO HORIZONTE","BOA VISTA","BRASÍLIA","CAMPO GRANDE","CUIABÁ",
-    "CURITIBA","FLORIANÓPOLIS","FORTALEZA","GOIÂNIA","JOÃO PESSOA","MACAPÁ","MACEIÓ","MANAUS","NATAL",
-    "PALMAS","PORTO ALEGRE","PORTO VELHO","RECIFE","RIO BRANCO","RIO DE JANEIRO","SALVADOR","SÃO LUÍS",
-    "SÃO PAULO","TERESINA","VITÓRIA"];
-
-    function myFunction() {
+   
+class Barracidades extends React.Component {
+  constructor(props){
+      super(props);
+      this.state ={
+          cidade: "BRASIL",
+      }
+  }
+  render(){
+      let cidade = ["FORTALEZA", "SALVADOR", "RECIFE"];
+      
+      function myFunction() {
         document.getElementById("myDropdown").classList.toggle("show");
         console.log("click!")
       }
@@ -35,72 +32,17 @@ class Barracidades extends Component {
           }
         }
     }
-
-    function nomeExibido (valor){
-        console.log(valor);
-        document.getElementById("myDropdown").classList.toggle("show");
-        return valor;
-    }
-    return (
+      return (
         <div>
-            <div class="dropdown">
-                <button onClick = {myFunction} class="dropbtn">BRASIL<img src={setabaixo} style={{float: "right", marginTop: "5px"}}/></button>
-                <div id="myDropdown" class="dropdown-content">
-                    <input type="text" placeholder="Digite uma cidade..." id="myInput" onKeyUp={filterFunction}/>
-                    {cidade.map((valor) => (<a onClick = {() => {nomeExibido(valor);}} id={valor}>{valor}</a>))}
-                </div>
+        <div class="dropdown">
+            <button onClick = {myFunction} class="dropbtn">{this.state.cidade}<img src={setabaixo} style={{float: "right", marginTop: "5px"}}/></button>
+            <div id="myDropdown" class="dropdown-content">
+                <input type="text" placeholder="Digite uma cidade..." id="myInput" onKeyUp={filterFunction}/>
+                {cidade.map((valor) => (<a class="opcoes" onClick = {(event) => {this.setState({cidade:valor}); myFunction();}} id={valor}>{valor}</a>))}
             </div>
         </div>
-    )
+    </div>
+      )    
+  }
 }
-
-export default Barracidades;
-*/    
-    function Barracidades (nomecidade) {
-    let cidade = ["BRASIL","ARACAJU","BELÉM","BELO HORIZONTE","BOA VISTA","BRASÍLIA","CAMPO GRANDE","CUIABÁ",
-    "CURITIBA","FLORIANÓPOLIS","FORTALEZA","GOIÂNIA","JOÃO PESSOA","MACAPÁ","MACEIÓ","MANAUS","NATAL",
-    "PALMAS","PORTO ALEGRE","PORTO VELHO","RECIFE","RIO BRANCO","RIO DE JANEIRO","SALVADOR","SÃO LUÍS",
-    "SÃO PAULO","TERESINA","VITÓRIA"];
-
-    function myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
-        console.log("click!")
-      }
-    
-    function filterFunction() {
-        var input, filter, ul, li, a, i, div, txtValue;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        div = document.getElementById("myDropdown");
-        a = div.getElementsByTagName("a");
-        for (i = 0; i < a.length; i++) {
-          txtValue = a[i].textContent || a[i].innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-          } else {
-            a[i].style.display = "none";
-          }
-        }
-    }
-
-    function nomeExibido (valor){
-        console.log(valor);
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-    
-            return (
-                <div>
-                    <div class="dropdown">
-                        <button onClick = {myFunction} class="dropbtn">CIDADE<img src={setabaixo} style={{float: "right", marginTop: "5px"}}/></button>
-                        <div id="myDropdown" class="dropdown-content">
-                            <input type="text" placeholder="Digite uma cidade..." id="myInput" onKeyUp={filterFunction}/>
-                            {cidade.map((valor) => (<a onClick = {() => {nomeExibido(valor);}} id={valor}>{valor}</a>))}
-                        </div>
-                    </div>
-                </div>
-            )
-        
-    }
-
-
 export default Barracidades;
--- src/login.js
+++ src/login.js
@@ -3,17 +3,29 @@ import './css/index.css';
 import Header from './header.js';
 import Footer from './footer.js';
 
-
-function Login() {
-    return (
-    <div className="Cadastro">
-        <html>
-            <Header/>
-            Login
-            <Footer/>
-        </html>
-    </div>
-    );
+class Login extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            palavra: "",
+        }
+    }
+    render(){
+        
+        return (
+            <div className="Cadastro">
+                        <html>
+                            <Header/>
+                            <input type="text" onChange={(event) => {this.setState({palavra:event.target.value})}}/>
+                            <input type="submit"></input>
+                            <h1>VC DIGITOU:{this.state.palavra}</h1>
+                            <Footer/>
+                        </html>
+                    </div>
+        )    
+    }
 }
+
+
 
 export default Login;
